f4c59c6b3d81257c3a5e4fbb289738dd9ac4f399
Luis Manuel Brito <lmbritor@pm.com>
2025-02-26 15:46:18 -0400

    ADD: Agrego funcionesde aide post y timer

--- ubuntu.sh
+++ ubuntu.sh
@@ -147,7 +147,7 @@ function main {
     done
     log_message "INFO: Successfully sourced all scripts from $script_dir"
 
-    # Define function execution list
+    # Define function execution list with updated AIDE functions
     local hardening_functions=(
         f_pre f_kernel f_firewall f_disablenet f_disablefs f_disablemod
         f_systemdconf f_resolvedconf f_logindconf f_journalctl f_timesyncd
@@ -155,9 +155,7 @@ function main {
         f_sudo f_logindefs f_sysctl f_limitsconf f_adduser f_rootaccess
         f_package_install f_psad f_coredump f_usbguard f_postfix f_apport
         f_motdnews f_rkhunter f_sshconfig f_sshdconfig f_password f_cron
-        f_ctrlaltdel f_auditd f_aide f_rhosts f_users f_lockroot
-        f_package_remove f_suid f_restrictcompilers f_umask f_path
-        f_aa_enforce f_aide_post f_aide_timer f_aptget_noexec f_aptget_clean
+        f_ctrlaltdel f_auditd f_aide f_aide_post f_aide_timer f_aptget_noexec f_aptget_clean  # CHANGED: Updated to f_aidepost and f_aidetimer
         f_systemddelta f_post f_checkreboot
     )
 
